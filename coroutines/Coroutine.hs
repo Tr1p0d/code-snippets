3bdd17ce57461908383d20cfb2239b6865091237
Marek 'Tr1p0d' Kidon <marek.kidon@itcommunity.cz>
2016-07-10 20:41:22 +0200

    Splitter and Join coroutines

--- coroutines/Coroutine.hs
+++ coroutines/Coroutine.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE LambdaCase #-}
+
 import Control.Monad (liftM)
 import Control.Monad.Trans (MonadTrans (..))
 import Data.Functor.Identity (Identity (..))
@@ -73,6 +76,8 @@ instance (Functor f, Functor g) => Functor (EitherT f g) where
 -- Transducers
 type Transducer a b m r = Coroutine (EitherT ((->) (Maybe a)) ((,) b)) m r
 
+data Void
+
 awaitT :: Monad m => Transducer a b m (Maybe a)
 awaitT = suspend $ LeftT return
 
@@ -107,4 +112,125 @@ bindM2 f m1 m2 = do
     r2 <- m2
     f r1 r2
 
+mapSuspension
+    :: (Monad m, Functor s)
+    => (forall a. s a -> s' a)
+    -> Coroutine s m a
+    -> Coroutine s' m a
+mapSuspension f cort = Coroutine {resume = liftM map' (resume cort)}
+  where
+    map' (Right r) = Right r
+    map' (Left s) = Left (f $ fmap (mapSuspension f) s)
 
+transducer1 :: Transducer Void Int IO ()
+transducer1 = trans' [1..10]
+  where
+    trans' [] = return ()
+    trans' (a:ax) = do
+        lift $ putStrLn $ "yielding: " ++ show a
+        yieldT a
+        trans' ax
+
+transducer2 :: Transducer Int Void IO ()
+transducer2 = do
+    dsVal <- awaitT
+    case dsVal of
+        Just x -> do
+            lift $ putStrLn $ "got: " ++ show x
+            transducer2
+        Nothing -> return ()
+
+toTrampoline :: Monad m => Transducer Void Void m r -> Trampoline m r
+toTrampoline = mapSuspension undefined
+
+-- Branching
+type Splitter a m r
+    = Coroutine (EitherT ((->) (Maybe a)) (EitherT ((,) a) ((,) a))) m r
+
+type Join a m r
+    = Coroutine (EitherT (EitherT ((->) (Maybe a)) ((->) (Maybe a))) ((,) a)) m r
+
+--newtype Coroutine s m r =
+--    Coroutine {resume :: m (Either (s (Coroutine s m r)) r)}
+yieldL :: (Monad m) => a -> Splitter a m ()
+yieldL a = suspend $ (RightT $ LeftT (a, return ()))
+
+yieldR :: (Monad m) => a -> Splitter a m ()
+yieldR a = suspend $ (RightT $ RightT (a, return ()))
+
+awaitL :: Monad m => Join a m (Maybe a)
+awaitL = suspend (LeftT $ LeftT return)
+
+awaitR :: Monad m => Join a m (Maybe a)
+awaitR = suspend (LeftT $ RightT return)
+
+splitter :: Splitter Int IO ()
+splitter = trans' [0..10]
+  where
+    trans' [] = return ()
+    trans' (a:aa:as) = yieldL a >> yieldR aa >> trans' as
+
+joiner :: Join Int IO ()
+joiner = do
+    lv <- awaitL
+    rv <- awaitR
+    case lv of
+        Just x -> do
+            lift $ putStrLn $ "got: " ++ show (rv, lv)
+            joiner
+        Nothing -> return ()
+
+joinTransduced
+    :: (Monad m)
+    => Transducer a b m x
+    -> Transducer a b m y
+    -> Join b m z
+    -> Transducer a b m (x,y,z)
+joinTransduced t1 t2 j =
+    Coroutine (bindM3 proceed (resume t1) (resume t2) (resume j))
+  where
+    -- | Left is waiting
+    proceed (Left (LeftT s)) c j = return (Left $ LeftT $ fmap
+            (\x -> joinTransduced x (Coroutine $ return c) (Coroutine $ return j)) s)
+
+    -- | Right is waiting
+    proceed c (Left (LeftT s)) j = return (Left $ LeftT $ fmap
+            (\x -> joinTransduced (Coroutine $ return c) x (Coroutine $ return j)) s)
+
+    -- | Join is yielding
+    proceed c1 c2 (Left (RightT s)) = return (Left $ RightT $ fmap
+            (\x -> joinTransduced (Coroutine $ return c1) (Coroutine $ return c2) x) s)
+
+    proceed (Left (RightT (x, c1))) c2 (Left (LeftT (LeftT f))) =
+        resume $ joinTransduced c1 (Coroutine $ return c2) (f $ Just x)
+
+    proceed c1 (Left (RightT (x, c2))) (Left (LeftT (RightT f))) =
+        resume $ joinTransduced (Coroutine $ return c1) c2 (f $ Just x)
+
+    proceed (Right x) c2 (Left (LeftT (LeftT f))) =
+        resume $ joinTransduced (return x) (Coroutine $ return c2) (f Nothing)
+
+    proceed c1 (Right y) (Left (LeftT (RightT f))) =
+        resume $ joinTransduced (Coroutine $ return c1) (return y) (f Nothing)
+
+    proceed (Right x) (Right y) (Right z) = return $ Right (x,y,z)
+
+joinTransducedTest = runTrampoline $ toTrampoline $ joinTransduced
+    leftT
+    rightT
+    joinTransducers
+    =>= return ()
+  where
+    leftT = yieldT "Vlevo" >> leftT =>= lift121 id
+    rightT = yieldT "Vpravo"
+    joinTransducers = do
+        y <- awaitR
+        lift $ putStrLn (show y)
+        joinTransducers
+
+bindM3 :: Monad m => (a -> b -> c -> m d) -> m a -> m b -> m c -> m d
+bindM3 f m1 m2 m3 = do
+    v1 <- m1
+    v2 <- m2
+    v3 <- m3
+    f v1 v2 v3
