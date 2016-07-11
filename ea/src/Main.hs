7250aa3a7ce68680f0d365eb165a6b579325d494
Marek 'Tr1p0d' Kidon <marek.kidon@itcommunity.cz>
2016-07-11 20:22:35 +0200

    Experimenting with pipeline

--- ea/src/Main.hs
+++ ea/src/Main.hs
@@ -1,77 +1,75 @@
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE RecordWildCards#-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
-import Prelude hiding ((++), length, replicate, init)
-import Control.Monad ((>=>))
-
+import Control.Monad.Coroutine
+import Control.Monad.Coroutine.SuspensionFunctors (Yield(Yield), Await(Await))
+import Control.Monad.Random (getRandomR)
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State.Lazy (StateT, execStateT, put)
-import Data.Vector as V (Vector, replicateM, unsafeFreeze, head, tail)
-import qualified Pipes.Prelude as PP
-import Pipes
-import Pipes.Core
-import Pipes.Concurrent
-import Pipes.Internal as PI
+import Data.Functor.Sum (Sum(InL, InR))
+import qualified Data.Vector as V (fromList, unsafeThaw, unsafeFreeze)
+import qualified Data.Vector.Mutable as MV (IOVector, length, read)
+import Data.Void (Void)
 
-import AI.EA.Genotype.Class.Genotype
-import AI.EA.Genotype.IntVector
+type GeneticPipeline a b m r = Coroutine (Sum (Await (Maybe a)) (Yield b)) m r
 
+type Selection b = GeneticPipeline Void b IO ()
 
-type PopulationSize = Int
-type Population a = V.Vector a
+type GeneticOp a b = GeneticPipeline a b IO ()
 
-type MonadEvolution s a = StateT s IO a
+type TournamentSize = Int
+type Population a = MV.IOVector a
 
--- | sort of it works
-main :: IO ()
-main = do
-    (GAIntMVector evolved) <- execStateT (evolve eParams) undefined
-    unsafeFreeze evolved >>= print
+tournamentSelection
+    :: TournamentSize
+    -> Population (a, Double)
+    -> Selection (a, Double)
+tournamentSelection tSize population = do
+    lift (getRandomIndividual >>= tournament (tSize - 1)) >>= yieldGP
+    tournamentSelection tSize population
   where
-    eParams = EvolutionParams
-        { populationSize = 10
-        , genotypeCrossParams = SinglePointC
-        , genotypeInitParams = GAInitIntVector 5 (-10, 10)
-        , genotypeMutateParams = Replace (-10, 10)
-        , fitness = undefined
-        , terminationCondition = undefined
-        }
+    getRandomIndividual = do
+        let upperI = MV.length population - 1
+        getRandomR (0, upperI) >>= MV.read population
 
-data EvolutionParams a = EvolutionParams
-    { populationSize :: PopulationSize
-    , genotypeInitParams :: InitParams a
-    , genotypeCrossParams :: CrossParams a
-    , genotypeMutateParams :: MutateParams a
-    , fitness :: a -> IO Double
-    , terminationCondition :: a -> IO ()
-    }
+    tournament 0 ind = return ind
+    tournament i ind@(_, fitness) = do
+        ind'@(_, fitness') <- getRandomIndividual
+        if fitness > fitness'
+        then tournament (i-1) ind'
+        else tournament (i-1) ind
 
-evolve :: (Genotype a) => EvolutionParams a -> MonadEvolution a ()
-evolve params = randomPopulation params >>= evolveWithPopulation params
+yieldGP :: Monad m => b -> GeneticPipeline a b m ()
+yieldGP = suspend . InR . flip Yield (return ())
 
-evolveWithPopulation
-    :: (Genotype a)
-    => EvolutionParams a
-    -> Population a
-    -> MonadEvolution a ()
-evolveWithPopulation params = lift . evolveWithPopulation' params >=> put
+awaitGP :: Monad m => GeneticPipeline a b m (Maybe a)
+awaitGP = suspend $ InL $ Await return
+
+(=>=)
+    :: Monad m
+    => GeneticPipeline a b m ()
+    -> GeneticPipeline b c m ()
+    -> GeneticPipeline a c m ()
+c1 =>= c2 = Coroutine (bindM2 proceed (resume c1) (resume c2))
   where
-    evolveWithPopulation' EvolutionParams{..} ePopulation = do
-        let e1 = V.head ePopulation
-            e2 = V.head $ V.tail ePopulation
-        fst <$> cross genotypeCrossParams e1 e2
+    proceed (Left (InL s)) c =
+        return (Left $ InL $ fmap (=>= Coroutine (return c)) s)
+    proceed c (Left (InR s)) =
+        return (Left $ InR $ fmap (Coroutine (return c) =>= ) s)
+    proceed (Left (InR (Yield b c1))) (Left (InL (Await f))) =
+        resume (c1 =>= f (Just b))
+    proceed (Left (InR (Yield b c1))) (Right y) =
+        resume (c1 =>= return y)
+    proceed (Right x) (Left (InL (Await f))) =
+        resume (return x =>= f Nothing)
+    proceed (Right x) (Right y) = return $ Right y
 
-randomPopulation
-    :: (Genotype a)
-    => EvolutionParams a
-    -> MonadEvolution a (Population a)
-randomPopulation EvolutionParams{..} =
-    lift $ V.replicateM populationSize $ init genotypeInitParams
+bindM2 :: Monad m => (a -> b -> m c) -> m a -> m b -> m c
+bindM2 f ma mb= do
+    va <- ma
+    vb <- mb
+    f va vb
 
+dummyPopulation :: IO (Population (Int, Double))
+dummyPopulation = V.unsafeThaw $ V.fromList $ zip [0..9] [10..19]
 
--- REPRODUCTION PIPELINE
+runGeneticPipeline :: Monad m => GeneticPipeline Void Void m r -> m r
+runGeneticPipeline = runCoroutine . mapSuspension undefined
 
-
+main = return ()
